8ed396997204d359afacef565325f4f4ebcc06ed
Emrecan <kursunemre01@gmail.com>
2024-07-18 17:13:44 +0300

    rename authenticationInterceptor => authorizationInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,13 +5,13 @@ import { routes } from "./app.routes";
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { provideAnimations } from "@angular/platform-browser/animations";
 import { provideToastr } from "ngx-toastr";
-import { authenticationInterceptor } from "./interceptors/authentication.interceptor";
+import { authorizationInterceptor } from "./interceptors/authorization.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authenticationInterceptor])),
+    provideHttpClient(withInterceptors([authorizationInterceptor])),
     provideAnimations(),
     provideToastr({
       positionClass: "toast-bottom-right",
--- src/app/interceptors/authorization.interceptor.ts
+++ src/app/interceptors/authorization.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from "@angular/common/http";
 import { inject } from "@angular/core";
 import { LocalStorageService } from "../services/local-storage.service";
 
-export const authenticationInterceptor: HttpInterceptorFn = (req, next) => {
+export const authorizationInterceptor: HttpInterceptorFn = (req, next) => {
   const localStorageService = inject(LocalStorageService);
   const accessToken = localStorageService.getAccessToken();
 
