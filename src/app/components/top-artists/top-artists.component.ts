6fe1963e5bd8a41ee8b13be3e20d971e59189d1b
Emrecan <kursunemre01@gmail.com>
2024-07-25 17:06:35 +0300

    create top-artists component

---
+++ src/app/components/top-artists/top-artists.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-artists',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-artists.component.html',
+  styleUrl: './top-artists.component.css'
+})
+export class TopArtistsComponent {
+
+}
